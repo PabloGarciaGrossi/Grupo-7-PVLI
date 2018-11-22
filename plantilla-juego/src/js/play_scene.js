2fe6c701306608a7f0f67987c52a889a7624b74d
PabloGarciaGrossi <pabgar20@ucm.es>
2018-11-22 17:14:30 +0100

    Mapa versión alfa

--- plantilla-juego/src/js/play_scene.js
+++ plantilla-juego/src/js/play_scene.js
@@ -7,24 +7,32 @@ var PlayScene = {
 
   create: function () {
     this.game.stage.backgroundColor = '#787878';
-    this.map = this.game.add.tilemap('map');
-    this.map.addTilesetImage('tileset');
-    this.map.setCollisionBetween(0,23);
-    this.layer = this.map.createLayer(0);
-   // this.map.setCollisionBetween(1, 5000, true, 0);
+    this.map = this.game.add.tilemap('mapa');
+    this.map.addTilesetImage('tilesInterior', 'tilesetCastillo');
+    //this.map.setCollisionBetween(0,23);
+    this.layer = this.map.createLayer('ground');
+    this.layer2 = this.map.createLayer('walls');
+    this.layer3 = this.map.createLayer('windows');
+    this.layer4 = this.map.createLayer('objects');
+    this.map.setCollisionBetween(1, 10000, true, 'walls');
+    this.map.setCollisionBetween(7192, 7194, false, 'walls');
+    this.map.setCollisionBetween(1, 10000, true, 'objects');
     this.layer.resizeWorld();
-    this.layer.debug = true;
+    this.layer2.resizeWorld();
+    this.layer3.resizeWorld();
+    this.layer4.resizeWorld();
+    //this.layer2.debug = true;
     this.distance = 40;
     
     
     this.cursors = this.game.input.keyboard.createCursorKeys();
     this.jugador = new Player(this.game,300,this.game.world.centerX,this.game.world.centerY,"player",this.cursors);
     this.enemy = new Enemy(this.game, 75, 150,150,"esqueleto");
-    this.rock = new RockRoll(this.game, 80, 500, 150, "rock", 0, 400);
+    //this.rock = new RockRoll(this.game, 80, 500, 150, "rock", 0, 400);
   
     this.jugador.create();
     this.enemy.create();
-    this.rock.create();
+    //this.rock.create();
     
     this.camera.follow(this.jugador);
 
@@ -32,13 +40,16 @@ var PlayScene = {
 
   update: function() {
     this.physics.arcade.collide(this.jugador,this.layer);
+    this.physics.arcade.collide(this.jugador,this.layer2);
+    this.physics.arcade.collide(this.jugador,this.layer3);
+    this.physics.arcade.collide(this.jugador,this.layer4);
     this.physics.arcade.collide(this.enemy, this.layer);
-    this.physics.arcade.collide(this.rock, this.layer);
+    //this.physics.arcade.collide(this.rock, this.layer);
 
     this.physics.arcade.overlap(this.jugador, this.enemy, this.collision, null, this);
     this.jugador.update();
     this.enemy.update(this.jugador.x, this.jugador.y);
-    this.rock.update(this.jugador.x, this.jugador.y);
+    //this.rock.update(this.jugador.x, this.jugador.y);
 
   },
 
